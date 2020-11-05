bdf8127c93725f60cc4c18932dbd793c976a6c0f
birajhalder1 <biraj.halder1992@gmail.com>
2020-11-05 23:24:10 +0530

    cart update

--- controller/cart.js
+++ controller/cart.js
@@ -4,14 +4,14 @@ const Product = require("../model/Product");
 
 exports.createSingleCart = async (req, res) => {
   try {
-
     let subT = 0;
-    for(let i = 0; i<= req.body.products.length - 1; i++){
-      let product = await Product.findOne({_id: mongoose.Types.ObjectId(req.body.products[i].product)});
-      if(product){
-        subT = subT + product.sellingPrice * req.body.products[i].quantity
+    for (let i = 0; i <= req.body.products.length - 1; i++) {
+      let product = await Product.findOne({
+        _id: mongoose.Types.ObjectId(req.body.products[i].product),
+      });
+      if (product) {
+        subT = subT + product.sellingPrice * req.body.products[i].quantity;
       }
-
     }
 
     const cartDetails = await new Cart({
@@ -20,7 +20,7 @@ exports.createSingleCart = async (req, res) => {
       totalPrice: subT,
       noOfItem: req.body.products.length,
       delivaryCharges: req.body.delivaryCharges,
-      subTotal: subT + req.body.delivaryCharges
+      subTotal: subT + req.body.delivaryCharges,
     }).save();
 
     res.status(201).json({
@@ -29,7 +29,7 @@ exports.createSingleCart = async (req, res) => {
       message: "Cart have created",
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res
       .status(400)
       .json({ success: false, message: "Internal server error" });
@@ -38,7 +38,6 @@ exports.createSingleCart = async (req, res) => {
 
 exports.getSingleCartWithId = async (req, res) => {
   try {
-    console.log("hitt");
     const cartId = req.params.cartId;
     const cart = await Cart.findOne({ _id: mongoose.Types.ObjectId(cartId) });
 
@@ -47,12 +46,78 @@ exports.getSingleCartWithId = async (req, res) => {
       data: cart,
     });
   } catch (error) {
-    console.log(error);
     return res
       .status(400)
       .json({ success: false, message: "Internal server error" });
   }
 };
+
+exports.updateCart = async (req, res) => {
+  try {
+    /**
+     * first create object
+     * update object fields
+     */
+
+    let subT = 0;
+    for (let i = 0; i <= req.body.products.length - 1; i++) {
+      let product = await Product.findOne({
+        _id: mongoose.Types.ObjectId(req.body.products[i].product),
+      });
+      if (product) {
+        subT = subT + product.sellingPrice * req.body.products[i].quantity;
+      }
+    }
+    const oCart = {
+      user: mongoose.Types.ObjectId(req.userData.id),
+      products: req.body.products,
+      totalPrice: subT,
+      noOfItem: req.body.products.length,
+      delivaryCharges: req.body.delivaryCharges,
+      subTotal: subT + req.body.delivaryCharges,
+    }
+
+    const updateCart = await Cart.findOneAndUpdate(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      { $set: oCart },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: updateCart,
+      message: "Cart has updated",
+    });
+  } catch (error) {
+    // console.log(error);
+    return res
+      .status(400)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+exports.deleteCart = async (req, res) => {
+  try {
+    const cart = await Cart.findByIdAndDelete({
+      _id: mongoose.Types.ObjectId(req.params.id),
+    });
+    res.status(200).json({
+      success: true,
+      data: cart,
+      message: "Cart has been removed !!",
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+/**
+ * Remove single product in product array
+ * update product array deleted field true
+ *
+ */
 
 exports.getAllByAdmin = async (req, res) => {
   try {
--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,15 @@
-const express = require("express")
-const { createSingleCart, getSingleCartWithId, getAllByAdmin } = require("../controller/cart")
-const {checkAuthCustomer, checkAuthAdmin} = require("../middleware/checkAuth")
+const express = require("express");
+const {
+  createSingleCart,
+  getSingleCartWithId,
+  updateCart,
+  deleteCart,
+  getAllByAdmin,
+} = require("../controller/cart");
+const {
+  checkAuthCustomer,
+  checkAuthAdmin,
+} = require("../middleware/checkAuth");
 
 const route = express.Router();
 
@@ -9,20 +18,34 @@ const route = express.Router();
  * desc     Create cart by customer
  * access   Private
  */
-route.post("/createCart", checkAuthCustomer , createSingleCart);
+route.post("/createCart", checkAuthCustomer, createSingleCart);
 
 /**
  * route    GET /api/v1/cart/:cartId
  * desc     Get single cart by customer
  * access   Private
  */
-route.get("/:cartId", checkAuthCustomer , getSingleCartWithId);
+route.get("/:cartId", checkAuthCustomer, getSingleCartWithId);
+
+/**
+ * route    PATCH /api/v1/cart/updateCart/:id
+ * desc     Update cart by customer
+ * access   Private
+ */
+route.patch("/updateCart/:id", checkAuthCustomer, updateCart);
+
+/**
+ * route    DELETE /api/v1/cart/deleteCart/:id
+ * desc     Delete cart by customer
+ * access   Private
+ */
+route.delete("/deleteCart/:id", checkAuthCustomer, deleteCart);
 
 /**
  * route    GET /api/v1/cart/allCartInfo
  * desc     Get all cart by admin
  * access   Private
  */
-route.get("/allCartInfo", checkAuthAdmin , getAllByAdmin);
+route.get("/allCartInfo", checkAuthAdmin, getAllByAdmin);
 
 module.exports = route;
